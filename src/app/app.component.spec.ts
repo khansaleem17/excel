f3a56fb6b31fb70caedbd8d2a8fb3a61709b30d2
saleem khan <saleemullakhan2s@gmail.com>
2021-07-04 17:51:56 +0530

    Excel sum calculator

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'list-rendering'`, () => {
+  it(`should have as title 'demo-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('list-rendering');
+    expect(app.title).toEqual('demo-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to list-rendering!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to demo-app!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, tap, combineAll } from 'rxjs/operators';
-import { AgGridAngular } from 'ag-grid-angular';
 
 
 
@@ -11,118 +10,64 @@ import { AgGridAngular } from 'ag-grid-angular';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  data: any[] = [];
-  private gridApi;
-  private gridColumnApi;
-  @ViewChild('agGrid', { static: false }) agGrid: AgGridAngular;
 
-  columnDefs = [
-    { headerName: 'ID', field: 'id', resizable: true, width: 400, rowDrag: true },
-    { headerName: 'Rule Name', field: 'ruleName', resizable: true, width: 400 },
-    { headerName: 'clone', field: 'isClone', width: 200, resizable: true }
-  ];
-  enableBtns: boolean;
-  selectedData: any;
-  selectedIndex: number;
-  upButtonDisabled = false;
-  downButtonDisabled = false;
-  showLoader = false;
-  constructor(private http: HttpClient) {
+  rows:any[] = [];
+  cols:any[] = [];
+  model = {}; 
+ 
+  constructor() {
   }
   ngOnInit() {
-    this.getData();
+   for (let index = 0; index < 10; index++) {
+     this.rows.push(index);
+     this.cols.push(index);
+   }
   }
-
-  getData() {
-    this.showLoader = true
-    // const data = localStorage.getItem('listData');
-    // if (data) {
-    //   this.data = JSON.parse(data);
-    //   return;
-    // }
-    this.http.get('http://jivoxdevuploads.s3.amazonaws.com/eam-dev/files/44939/Rule%20JSON.json').pipe(
-      catchError(e => {
-        this.showLoader = false
-        alert('something went wrong!')
-        return e;
-      }),
-      tap(response => {
-        this.showLoader = false
-        if (response && response['data']) {
-          // const resData = JSON.stringify(response['data']);
-          // localStorage.setItem('listData', resData);
-          this.data = response['data']
-        }
-      })
-    ).subscribe();
+assignvalue(row,col,ev){
+  const key = this.getKey(row,col);
+  const value:String = ev.target.value;
+  if(!isNaN(value)){
+  this.model[key] = {
+    value:ev.target.value ? parseFloat(ev.target.value) : null,
+    col:col,
+    row:row
+  };
   }
+};
 
-
-  deleteRow() {
-    if (this.checkSelected()) {
-      this.gridApi.updateRowData({ remove: [this.selectedData[0].data] });
-      alert('row deleted successfully');
-      this.enableBtns = false;
-    }
-
-  }
-  rowSelected(e) {
-    this.upButtonDisabled = this.downButtonDisabled = false;
-    this.selectedData = this.gridApi.getSelectedNodes();
-    if (this.selectedData && this.selectedData.length) {
-      if (this.selectedData[0].rowIndex === 0) {
-        this.upButtonDisabled = true;
+calculateSum(row,col,ev){
+  const key = this.getKey(row,col);
+  const value:String = ev.target.value;
+   if(value.toLowerCase().includes('=')){
+    let strArray = value.split(',');
+   strArray = strArray.map(s => {
+      s = s.trim();
+      if( s.includes('=')) {
+       s = s.replace('=','');
       }
-      if (this.selectedData[0].rowIndex === this.data.length - 1) {
-        this.downButtonDisabled = true;
-      }
-      this.enableBtns = true;
-    }
+      return s;
+    })
+    console.log(strArray);
+    let selectedRows = [];
+    strArray.forEach(k => {
+      let row = this.model[this.getKey(k,col)];
+      selectedRows.push(row);
+    });
+   let sum = (selectedRows.map(x => x.value)).reduce((acc,curr) => {
+    return acc + curr
+   },0);
+   console.log(sum);
+    ev.target.value = sum;
+    this.model[key] = {
+      value:sum,
+      col:col,
+      row:row
+    };
+    console.log(sum);
   }
-  onReady(params) {
-    this.gridApi = params.api;
-    this.gridColumnApi = params.columnApi;
-  }
-  clone() {
-    if (this.checkSelected()) {
-      const newObj = { ...this.selectedData[0].data, isClone: true }
-      this.gridApi.updateRowData({
-        add: [newObj],
-        addIndex: this.selectedData[0].rowIndex + 1
-      });
-    }
-  }
-  checkSelected() {
-    if (!(this.selectedData && this.selectedData.length)) {
-      alert('Select a row to perform this operation');
-      return false;
-    }
-    return true;
+}
 
-  }
-  moveInArray(arr, fromIndex, toIndex) {
-    const element = arr[fromIndex];
-    arr.splice(fromIndex, 1);
-    arr.splice(toIndex, 0, element);
-  }
-
-  moveUpDown(type) {
-    if (this.checkSelected()) {
-      if (type === 'down' && this.downButtonDisabled) {
-        return;
-      }
-      if (type === 'up' && this.upButtonDisabled) {
-        return;
-      }
-      const rowIndex = this.selectedData[0].rowIndex;
-      const toIndex = type === 'down' ? rowIndex + 1 : rowIndex - 1;
-      const newStore = this.data.slice();
-      this.moveInArray(newStore, rowIndex, toIndex);
-      this.data = newStore;
-      this.gridApi.setRowData(newStore);
-      this.selectedData = [];
-      this.gridApi.clearFocusedCell();
-    }
-  }
-
+getKey(row,col){
+return `col_${col}_row_${row}`
+}
 }
